b675bba7159b7c3e1b6e6a6ab41007d20089000d
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-03-05 18:50:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
-const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
 const app = express();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./Chinook_Sqlite_AutoIncrementPKs.sqlite');
 const Sequelize = require('sequelize');
+const port = process.env.PORT || 3000;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var user = require('../models/user');
+
+const models = require('./models');
+const users = require('./routes/users');
+const posts = require('./routes/posts');
 
 //Engines
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', process.env.PORT || 3000);
+
 
 //BodyParsers
 app.use(bodyParser.json());
@@ -34,24 +34,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ secret: 'secret' }));
 
 //Passport Init
+require("./config/passport")(passport);
 app.use(passport.initialize());
 app.use(passport.session());
-
-
-
-//Header
-// var http = require('http');
-// var options = { method: 'HEAD', port: 3000, path: '/' };
-// var req = http.request(options, function(res) {
-//   console.log(res.headers);
-// });
-// req.end();
 
 //Connect Flash
 app.use(flash());
 
 //Global Vars
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('err');
@@ -60,40 +51,18 @@ app.use(function (req, res, next) {
 
 
 
-Artist.hasMany(Album, { foreignKey: "ArtistId" });
-Album.belongsTo(Artist, { foreignKey: "ArtistId" });
-
-app.get('/album', (request, response) => {
-  response.header({
-    DBnumber: 01,
-    Music: true
-  });
-  if (request.header('music-request') === 'album-artist') {
-    Album.findAll({
-      include: [
-        {
-          model: Artist
-        }
-      ]
-    }).then(albumArtist => {
-      response.json(albumArtist);
-    });
-  } else if (request.header('music-request') === 'album') {
-    Album.findAll().then(albums => {
-      response.json(albums);
-    });
-  }
-});
-
 //routes
-app.use('/', routes);
+// app.use('/', routes);
 app.use('/users', users);
+app.use('/posts', posts);
 
 app.use((req, res) => {
   res.status(400);
   res.render('404');
 });
 
-app.listen(3000, () => {
-  console.log('server running')
+models.sequelize.sync().then(function() {
+  app.listen(port, function() {
+    console.log('server running on ' + port);
+  });
 });
