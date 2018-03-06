92a2b6e3ffc52c2b5cd93ff99f01bc891c20d5f1
tjb1272 <30851416+tjb1272@users.noreply.github.com>
2018-03-05 18:47:05 -0700

    Update and rename index.js to posts.js

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//Home
-router.get('/', (req, res) => {
-    res.render('home');
-});
-
-module.exports = router;
---
+++ routes/posts.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated } = require('../authorize/auth');
+const post = require('../models').post;
+
+router.post('/register', ensureAuthenticated, (req, res) => {
+  post.create({ title: req.body.title, UserId: req.user.id }).then(result => {
+    res.status(200).json({
+      message: 'Post create',
+      post: result
+    });
+  });
+});
+
+
+module.exports = router;
